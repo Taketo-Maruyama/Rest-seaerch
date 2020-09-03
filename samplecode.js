83b459eef623acc009c97cfd058224db299db0c5
Taketo-Maruyama <taketomaruyama0128@gmail.com>
2020-09-03 20:49:56 +0900

    レストラン検索結果の表示を調整

--- samplecode.js
+++ samplecode.js
@@ -15,9 +15,39 @@ function successCallback(position) {
 
   var showResult = result => {
     result.rest.map( item => {
-      $("#table").append(`<tr><td><img src="${item.image_url.shop_image1}"></td><td>${item.name}</td><td>${item.access.station}</td><td>${item.access.walk}分</td><td><a href="${item.url}" target="_blank">${item.url}</a></td></tr>`)
+      /*$("#table").append(`<tr><td><img src="${item.image_url.shop_image1}"></td><br>
+      <td>${item.name}</td><br><td>${item.access.station}</td><td>${item.access.walk}分</td><br>
+      <td><a href="${item.url}" target="_blank">${item.url}</a></td></tr>`)*/
+
+      $("#shopcard").append(`
+        <center>
+        <img class="card-img"  src="${item.image_url.shop_image1}">
+        </center>
+         <div class="card-content"><h1 class="card-title">${item.name}</h1>
+           <p class="card-text">アクセス:${item.access.station}から${item.access.walk}分</p>
+         </div>
+         <div class="card-link">
+           <a href="${item.url}" target="_blank">About</a>
+         </div><br>
+        `)
     })
   }
+
+  /*<img class="card-img"  src="${item.image_url.shop_image1}">
+   <div class="card-content"><h1 class="card-title">${item.name}</h1>
+     <p class="card-text">アクセス:${item.access.station}から${item.access.walk}分</p>
+   </div>
+   <div class="card-link">
+     <a href="${item.url}" target="_blank">About</a>
+   </div>*/
+
+   /*
+   <img class="card-img-top" src="${item.image_url.shop_image1}" alt="Card image cap">
+   <div class="card-block">
+     <h4 class="card-title">${item.name}</h4>
+     <p class="card-text">アクセス:${item.access.station}から${item.access.walk}分</p>
+     <a href="${item.url}" target="_blank" >About</a>
+   */
 
   $( function () {
     const url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
@@ -26,17 +56,30 @@ function successCallback(position) {
       latitude:  "",
       longitude: "",
       range: "",
-      hit_per_page: ""
+      hit_per_page: "",
     }
 
 
-      params.latitude = gl_lati  //緯度をgeolocationで取得してその値を入れる
-      params.longitude = gl_long //経度をgeolocationで取得してその値を入れる
+      params.latitude = 34.6661 //gl_lati  //緯度をgeolocationで取得してその値を入れる
+      params.longitude = 133.9177 //gl_long //経度をgeolocationで取得してその値を入れる
       params.range = $("#distance").val()  //検索範囲は選択されたものがここに代入される
       params.hit_per_page = 100 //1ページあたりの表示件数
       $.getJSON( url, params, result => {
-        showResult( result )
-      })
+          showResult( result )
+          console.log(result.total_hit_count)
+        })
+      /*while (i * 100 < result.total_hit_count){
+        i++
+        params.latitude = 34.6661 //gl_lati  //緯度をgeolocationで取得してその値を入れる
+        params.longitude = 133.9177 //gl_long //経度をgeolocationで取得してその値を入れる
+        params.range = $("#distance").val()  //検索範囲は選択されたものがここに代入される
+        params.hit_per_page = 100 //1ページあたりの表示件数
+        params.offset_page = i
+        $.getJSON( url, params, result => {
+            showResult( result )
+          })
+      }*/
+
 
   })
 }
@@ -58,5 +101,7 @@ function errorCallback(error) {
   }
   document.getElementById("show_result").innerHTML = err_msg;
   //デバッグ用→　document.getElementById("show_result").innerHTML = error.message;
+
+
   }
 }
